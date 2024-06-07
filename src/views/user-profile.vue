6af282fe447f4ef3dbc7175eb7b4f2170c02bd8d
Cristhian Orosco <u201921031@upc.edu.pe>
2024-06-07 16:12:54 -0500

    feat: add profile container in user profile

---
+++ src/views/user-profile.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="profile-container">
+      <h1>Hola Enrique</h1>
+      <div class="profile-card">
+        <img class="profile-pic" src="https://via.placeholder.com/150" alt="Profile Picture">
+        <div class="profile-info">
+          <div class="info-row">
+            <label>Nombres</label>
+            <InputText v-model="user.nombres" />
+          </div>
+          <div class="info-row">
+            <label>Apellidos</label>
+            <InputText v-model="user.apellidos" />
+          </div>
+          <div class="info-row">
+            <label>Dirección</label>
+            <InputText v-model="user.direccion" />
+          </div>
+          <div class="info-row">
+            <label>Distrito</label>
+            <InputText v-model="user.distrito" />
+          </div>
+          <div class="info-row">
+            <label>Número</label>
+            <InputText v-model="user.numero" />
+          </div>
+          <div class="info-row">
+            <label>Edad</label>
+            <InputText v-model="user.edad" />
+          </div>
+          <div class="info-row">
+            <label>Género</label>
+            <InputText v-model="user.genero" />
+          </div>
+          <div class="info-row">
+            <label>Tipo de Usuario</label>
+            <InputText v-model="user.tipoUsuario" />
+          </div>
+        </div>
+        <Button label="Editar Perfil" icon="pi pi-user-edit" class="edit-button" />
+      </div>
+    </div>
+  </template>
